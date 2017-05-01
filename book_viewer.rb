b99c72e9b027da873fece860b6871869d55eac1c
Fernanda Franco <francodias.fernanda@gmail.com>
2017-05-01 14:18:55 -0400

    all files

---
+++ book_viewer.rb
@@ -0,0 +1,17 @@
+require "tilt/erubis"
+require "sinatra"
+require "sinatra/reloader"
+
+get "/" do
+  @title = "The Adventures of Sherlock Holmes"
+  @table_of_contents = File.readlines("data/toc.txt")
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @table_of_contents = File.readlines("data/toc.txt")
+  @chapter_text = File.read("data/chp1.txt")
+
+  erb :chapter
+end
